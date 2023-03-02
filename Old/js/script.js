c35f2d92734e5f89237b6d11903630e7580a0360
Dan <atibudan2@gmail.com>
2023-03-02 23:19:58 +0100

    update script.js file

--- Old/js/script.js
+++ Old/js/script.js
@@ -19,3 +19,18 @@ function callback(entries, observer) {
 }
 const observer = new IntersectionObserver(callback, options);
 window.addEventListener("load", () => document.querySelectorAll(".no_vsb").forEach(element => observer.observe(element)));
+
+//
+const templates = document.querySelectorAll(".template");
+document.querySelectorAll(".home_nav_link").forEach(link => {
+    link.onclick = e => {
+        e.stopPropagation();
+        templates.forEach(template => {
+            let link_ = e.target.getAttribute("href").split("#")[1];
+            if (template.getAttribute("class").includes(link_)) {
+                return template.classList.remove("tpl-ivb");
+            };
+            template.classList.add("tpl-ivb");
+        })
+    };
+});
